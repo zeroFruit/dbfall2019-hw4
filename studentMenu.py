d256c831a5dd36e3c5b34e0fbda114cbf0a8222c
steve806 <steve806@snu.ac.kr>
2019-10-17 11:07:24 +0900

    almost done

--- studentMenu.py
+++ studentMenu.py
@@ -20,22 +20,21 @@ grades_to_point = {
 
 def student_menu():
 
+    # 메뉴 UI 출력 및 동작에 해당하는 함수 출력
     menu_num = -1
 
     while menu_num != '0':
         print("\n\nWelcome %s" % user_acc.name)
         print("Please select student menu")
         print("1) Student Report")
-        print("2) Check Course Qualification")
-        print("3) View Time Table")
+        print("2) View Time Table")
         print("0) Quit")
         menu_num = input("Enter : ")
 
         switcher = {
             '0': quit_menu,
             '1': print_stud_report,
-            '2': print_course_qual,
-            '3': print_time_table
+            '2': print_time_table
         }
 
         selected_func = switcher.get(menu_num, print_wrong)
@@ -70,8 +69,7 @@ def print_stud_report():
     print("You have taken total %s credits\n" % data[3])
     print("Semester report\n")
 
-    # 평점 구하는 과정
-
+    # 해당 학생이 수강한 year, semester 쌍을 시간 순서대로 모두 출력
     c.execute("SELECT year, semester "
               "FROM takes "
               "WHERE ID = \"%s\" "
@@ -95,6 +93,7 @@ def print_stud_report():
 
         gps = []
 
+        # TODO: GPA가 null인 경우 핸들링 필요
         ind = 0
         for grade in grades:
             for i in range(int(credit[ind])):
@@ -123,48 +122,48 @@ def print_stud_report():
     return
 
 
-def print_course_qual():
-
-    c = user_acc.conn.cursor()
-
-    while(True):
-        print("\nCheck Course Qualification")
-        course_info = input("Enter course ID or Title (Enter q to quit)")
-
-        if(course_info == "q" or course_info == "Q"):
-            break
-
-        # Select course_id from course where title = input
-
-        # If course id가 없다면
-        #   input은 title이 아니라 course_id 일 수도 있다.
-        #   Select title from course where course_id = input
-        #   if title이 없다면
-        #       해당 course는 존재하지 않는다 출력
-        #       return
-        #   else: #input이 course_id일 때
-        #       course_id = input
-        #       course_title = title
-        # else: #input이 title일 때
-        #   course_id = course_id
-        #   course_title = input
-
-
 def print_time_table():
 
     c = user_acc.conn.cursor()
 
-    # user가 수강한 year, semester 정보 받아오기
-    # Takes table을 이용하여 user가 수강한 year, semester쌍을 최근 순서로 받아오기 (distinct로 중복계산 방지)
+    # 해당 학생이 수강한 year, semester 쌍을 시간 순서대로 모두 출력
+    c.execute("SELECT year, semester "
+              "FROM takes "
+              "WHERE ID = \"%s\" "
+              "GROUP BY year, semester "
+              "ORDER BY year DESC, semester"
+              % user_acc.ID)
+
+    results = c.fetchall()
+
+    print("Please select semester to view")
+
+    i = 0
+    for result in results:
+        i = i + 1
+        print("%2s) %s %s" % (i, result[0], result[1]))
+
+    semester_num = int(input())
+
+    year, semester = results[semester_num - 1]
 
     print("\nTime Table\n")
-    print("%10s\t%40s\t%15s\t%10s\t%10s" % ("course_id", "title", "day", "start_time", "end_time"))
+    print("%10s\t%40s\t%15s\t%10s\t%10s"
+          % ("course_id", "title", "day", "start_time", "end_time"))
 
-    # user가 수강한 year, semester중 가장 최근 year, semester를 이용하여
-    # time table 만들기
-    # Takes, course, section, time_slot 을 natural join하여 사용자가 수강한 강의의 course_id, title과 그 강의의 시작과 끝 시간을 받아온다.
-    # For course_time in course_times:
-    #   위 형식에 맞춰 course_id, title, day, start_time, end_time 출력
+    c.execute("select course_id, title, day, start_hr, start_min, end_hr, end_min "
+              "from takes natural join course natural join section natural join time_slot "
+              "where ID = %s and semester = \"%s\" and year = \"%s\""
+              % (user_acc.ID, semester, year))
+
+    course_times = c.fetchall()
+
+    for course_time in course_times:
+        course_id, title, day, start_hr, start_min, end_hr, end_min = course_time
+        start_time = "%02d : %02d" % (start_hr, start_min)
+        end_time = "%02d : %02d" % (end_hr, end_min)
+        print("%10s\t%40s\t%15s\t%10s\t%10s"
+              % (course_id, title, day, start_time, end_time))
 
     # 사용한 cursor  닫기
     c.close()
