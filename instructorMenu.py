44f733197639cae4bbe1a09381988871c7a8f68c
LiverselleR <madscientist6792@gmail.com>
2019-10-23 00:24:09 +0900

    1-5 done

--- instructorMenu.py
+++ instructorMenu.py
@@ -10,11 +10,16 @@ def instructor_menu():
         print("\n\nWelcome %s" % user_acc.name)
         print("select instructor menu")
 
+        print("1) Course report")
+        print("2) Advisee report")
         print("0) Quit")
+
         menu_num = input("Enter : ")
 
         switcher = {
-            '0': quit_menu
+            '0': quit_menu,
+            '1': course_report,
+            '2': advisee_report,
         }
 
         selected_func = switcher.get(menu_num, print_wrong)
@@ -33,6 +38,52 @@ def quit_menu():
     del user_acc
 
     return
+
+
+def course_report():
+    pass
+
+
+def advisee_report():
+
+    c = user_acc.conn.cursor()
+
+    ## instructor 가 advise 한 학생의 id를 advisor 에서 찾는다.
+    c.execute("SELECT s_ID "
+              "FROM advisor "
+              "WHERE i_ID = \"%s\" "
+              % user_acc.ID)
+
+    s_IDs = c.fetchall()
+
+    ## 찾은 학생들의 ID에 해당하는 정보를 출력한다.
+
+    if s_IDs is None:
+        print("\nAdvised Student not exist")
+        pass
+
+    print("\n%10s\t%15s\t%20s\t%10s"
+          % ("ID", "name", "dept_name", "tot_cred"))
+
+    for s_id in s_IDs:
+
+        c.execute("SELECT * "
+                  "FROM student "
+                  "WHERE ID = \"%s\" "
+                  % s_id)
+        
+        std_data = c.fetchone()
+
+        std_id, std_name, std_dept, std_cred = std_data
+
+        print("%10s\t%15s\t%20s\t%10s"
+                % (std_id, std_name, std_dept, std_cred))
+
+
+    ## 사용한 cursor  닫기
+    c.close()
+
+    pass
 
 
 def print_wrong():
